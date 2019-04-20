4d329df81b9b9b6eb62de183edd8c5e176dfc0f5
Thiago Silva <thiagowfer@gmail.com>
2019-04-20 18:09:52 -0300

    Criação da tabela estática

--- index.php
+++ index.php
@@ -3,6 +3,30 @@
 include_once "includes/header.php";
 ?>
 
+<div class="row">
+	<div class="col s12 m6 push-m3">
+		<h3 class="light">Clientes</h3>
+		<table class="striped">
+			<thead>
+			<tr>				
+				<th>Nome: </th>
+				<th>Sobrenome:</th>
+				<th>Email:</th>
+				<th>Idade:</th>			
+			</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Thiago</td>
+					<td>Ferreira</td>
+					<td>thiagowfer@gmail.com</td>
+					<td>31</td>
+				</tr>
+			</tbody>
+		</table>	
+	</div>
+</div>
+
 <?php
 //Footer
 include_once "includes/footer.php";
