ccb85efe22fb3a6bb0849994fb6c663567cacf03
Thiago Silva <thiagowfer@gmail.com>
2019-04-22 20:26:44 -0300

    Página editar listando clientes

--- index.php
+++ index.php
@@ -30,7 +30,8 @@ include_once "includes/message.php";
 					<td><?php echo ucwords($dados['sobrenome']); ?></td>
 					<td><?php echo $dados['email']; ?></td>
 					<td><?php echo $dados['idade']; ?></td>
-					<td><a href="" class="btn-floating orange"><i class="material-icons">edit</i></a></td>
+					<td><a href="editar.php?id=<?php echo $dados['id'];?>" 
+						class="btn-floating orange"><i class="material-icons">edit</i></a></td>
 					<td><a href="" class="btn-floating red"><i class="material-icons">delete</i></a></td>
 				</tr>
 			</tbody>
