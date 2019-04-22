74c2a056bcb90596e3d8a94a4115ec780ba5008c
Thiago Silva <thiagowfer@gmail.com>
2019-04-22 19:44:07 -0300

    Criado o select e exibindo os resultados na página inicial

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
 //Header
 include_once "includes/header.php";
+include_once "php_action/db_connect.php";
 ?>
 
 <div class="row">
@@ -16,15 +17,21 @@ include_once "includes/header.php";
 			</tr>
 			</thead>
 			<tbody>
+				<?php
+				$sql = "SELECT * FROM clientes ORDER BY nome";
+				$resultado = mysqli_query($connect, $sql);
+				while($dados = mysqli_fetch_array($resultado)):
+				?>
 				<tr>
-					<td>Thiago</td>
-					<td>Ferreira</td>
-					<td>thiagowfer@gmail.com</td>
-					<td>31</td>
+					<td><?php echo ucwords($dados['nome']); ?></td>
+					<td><?php echo ucwords($dados['sobrenome']); ?></td>
+					<td><?php echo ucwords($dados['email']); ?></td>
+					<td><?php echo $dados['idade']; ?></td>
 					<td><a href="" class="btn-floating orange"><i class="material-icons">edit</i></a></td>
 					<td><a href="" class="btn-floating red"><i class="material-icons">delete</i></a></td>
 				</tr>
 			</tbody>
+				<?php endwhile; ?>
 		</table>	
 		<br>
 		<a href="adicionar.php" class="btn">Adicionar Cliente</a>
